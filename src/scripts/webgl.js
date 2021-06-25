38bf50abc8f4c39c1fc6d18911e026ac02b3170c
Sam Rosen <rosen.samuel@gene.com>
2021-06-25 13:39:30 -0400

    Add skeleton webgl builders

--- src/scripts/webgl.js
+++ src/scripts/webgl.js
@@ -1,3 +1,8 @@
+const baseVertexShader = `
+  attribute vec4 aVertexPosition;
+  uniform lowp float pointSizeModifier;
+`;
+
 /**
  * A vertex shader meant to take in positions and colors.
  */
@@ -42,8 +47,70 @@ const varyingColorsFragmentShader = `
 `;
 
 class VertexShaderBuilder {
-  constructor(options) {
-    this.shader = "";
+  constructor() {
+    this.uniforms = [];
+    this.attributes = [];
+  }
+
+  addChannelBuffer(channel) {
+    this.shader += `attribute lowp float ${channel};\n`;
+    this.attributes.add();
+  }
+
+  setChannelUniform(channel, uniform) {
+    this.shader += `uniform lowp float ${channel};\n`;
+    return this;
+  }
+
+  buildShader() {
+    this.shader += `
+      varying lowp vec4 vColor;
+
+      vec3 unpackColor(float f) {
+        vec3 color;
+        color.r = floor(f / 65536.0);
+        color.g = floor((f - color.r * 65536.0) / 256.0);
+        color.b = floor(f - color.r * 65536.0 - color.g * 256.0);
+        return color / 256.0;
+      }
+
+      void main(void) {
+        gl_Position = aVertexPosition;
+        vec3 unpackedValues = unpackColor(aVertexColor);
+    
+        vColor = vec4(
+          unpackedValues.rgb,
+          opacity
+        );
+        gl_PointSize = pointsModifier * pointSize;
+      }
+    `;
+  }
+
+  getShaderDetails() {}
+
+  static fromSchema(schema) {
+    // Returns one per track
+    return schema.tracks.map(VertexShaderBuilder.fromTrack);
+  }
+
+  static fromTrack(track) {}
+}
+
+
+class WebGLProgramBuilder {
+  constructor()
+
+  bindBuffer(bufferArray) {
+
+  }
+
+  specifyPointers(bufferArray);
+
+  getProgramInfo();
+
+  static fromSchema(schema) {
+    const shaders = VertexShaderBuilder.fromSchema(schema);
   }
 }
 
