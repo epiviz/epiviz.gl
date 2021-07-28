1c7492e4240bb24afbc675dd703d44698eceffec
Sam Rosen <rosen.samuel@gene.com>
2021-07-28 16:30:32 -0400

    Remove irrelevant note on webworker bundling

--- rollup.config.js
+++ rollup.config.js
@@ -9,13 +9,3 @@ export default {
   },
   plugins: [OMT(), nodeResolve()],
 };
-
-/**
- * Note: Currently we just copy the webworker files into the dist directory, so they do not get
- * optimized. This seems to work fine, but a more permananent solution would be to update the
- * webworkers to be imported like the plugin recommends:
- *
- * https://github.com/darionco/rollup-plugin-web-worker-loader
- *
- * This is not done at the moment to avoid issues with parcel
- */
