eef48404b2dc6a91175ad03c48f0a2ceeeb13d9b
Sam Rosen <rosen.samuel@gene.com>
2021-07-28 14:48:00 -0400

    Remove axios and use fetch instead

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
 import WebGLVis from "./epiviz.gl/webgl-vis";
 
-export { WebGLVis };
-// export WebGLVis;
-// const y = 4;
-// export default y;
-
-// export {default as y}
-// module.exports = WebGLVis;
-
-// // export = WebGLVis;
+export default WebGLVis;
