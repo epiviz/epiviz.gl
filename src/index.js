310aceb3ba995a78ae7dee088707a582ea293fff
Ossama Rafique <contact@ossamarafique.com>
2024-05-17 19:02:26 -0400

    Export WebGL Classes
    Expanded module exports for WebGL functionalities.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+import DataProcessor from "./epiviz.gl/data-processor";
+import Drawer from "./epiviz.gl/drawer";
+import GenomeScale from "./epiviz.gl/genome-scale";
+import GeometryMapper from "./epiviz.gl/geometry-mapper";
+import MouseReader from "./epiviz.gl/mouse-reader";
+import OffscreenWebGLDrawer from "./epiviz.gl/offscreen-webgl-drawer";
+import SemanticZoomer from "./epiviz.gl/semantic-zoomer";
+import SpecificationProcessor from "./epiviz.gl/specification-processor";
+import SVGInteractor from "./epiviz.gl/svg-interactor";
+import utilities from "./epiviz.gl/utilities";
+import WebGLCanvasDrawer from "./epiviz.gl/webgl-drawer";
 import WebGLVis from "./epiviz.gl/webgl-vis";
 
 export default WebGLVis;
+
+export {
+  DataProcessor,
+  Drawer,
+  GenomeScale,
+  GeometryMapper,
+  MouseReader,
+  OffscreenWebGLDrawer,
+  SVGInteractor,
+  SemanticZoomer,
+  SpecificationProcessor,
+  WebGLCanvasDrawer,
+  WebGLVis,
+  utilities,
+};
